da4527c5310603a8b3678e3caa2beb8b2455f2c6
Wesley Camargo <camargo.wes@gmail.com>
2022-11-19 20:40:00 +0100

    Feature/loganalytics (#12)
    * log analytics workspace
    * fixing pipeline on pr

---
+++ src/iac/az-modules/az-resources/Microsoft.OperationalInsights/logAnalyticsWorkspace/README.md
@@ -0,0 +1,24 @@
+## Purpose
+
+This module provision an Azure Log Analytics Workspace.
+
+## Requirements
+
+| Name      | Version |
+| --------- | ------- |
+| Bicep CLI | 0.10.61 |
+
+## Inputs
+
+| Name     | Description                             | Type   | Default               | Required | Size | Allowed values |
+| -------- | --------------------------------------- | ------ | --------------------- | :------: | ---- | -------------- |
+| name     | Resource Group name                     | string |                       |   yes    |      |                |
+| location | Resource Group location                 | string | deployment().location |   yes    |      |                |
+| acrSku   | A tier of your Azure Container Registry | string | Basic                 |    no    |      |                |
+| tags     | Resource Group tags                     | object |                       |    no    |      |                |
+
+## Outputs
+
+| Name        | Description                             |
+| ----------- | --------------------------------------- |
+| loginServer | The login server property for later use |
---
+++ src/iac/az-modules/az-resources/Microsoft.OperationalInsights/logAnalyticsWorkspace/tests/logAnalyticsWorkspace.tests.ps1
@@ -0,0 +1,40 @@
+BeforeAll {
+
+    # TODO: Load modules
+
+    $Context = @{
+        # WorkloadAffix    = "wl"
+        # ApplicationSufix = "pst"
+        # Environment      = "exp"
+        Template      = "../main.bicep"
+        ResourceGroup = "rg-bicepmodules"
+    }
+}
+
+Describe "Log Analytics Workspace" -Tag logAnalyticsWorkspace {
+    Context "Validate Log Analytics Workspace" {
+
+        It "Deployment must be sucessfull" -Skip {
+            # $deployment = New-AzDeployment  -Location westeurope `
+            #     -TemplateFile $Context.Template `
+            #     -workloadAffix $Context.WorkloadAffix `
+            #     -applicationSufix $Context.ApplicationSufix `
+            #     -environment $Context.Environment
+            $deployment = az deployment group create `
+                --resource-group $Context.ResourceGroup `
+                --template-file $Context.Template | ConvertFrom-Json
+
+            $deploymentState = $deployment.properties.provisioningState
+
+            $deploymentState | Should -Be "Succeeded"
+        }
+
+        It "Log Analytics must no be null" {
+            $logAnalytics = az monitor log-analytics workspace list `
+                --resource-group $Context.ResourceGroup `
+            | ConvertFrom-Json -AsHashtable
+
+            $logAnalytics | Should -Not -Be $null
+        }
+    }
+}
